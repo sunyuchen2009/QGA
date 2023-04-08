89f995f1bce0d52b6ec6fe8c51469ab895581f45
syc <sunyuchen2009@163.com>
2023-04-08 14:54:17 +0800

    添加项目文件。

---
+++ QGA/QGA.cpp
@@ -0,0 +1,512 @@
+﻿#include "QGA.h"
+using namespace std;
+
+//生成[0, 1]之间的随机数
+double srand() {
+    int N = rand() % 999;
+    double random = static_cast<double>(N) / 1000.0;;//随机产生0到1的小数
+    //cout << random << endl;
+    return random;
+}
+
+/*
+* 初始化种群，使用量子比特编码
+* @param M 种群大小 
+* @param N 每个基因的量子比特编码长度
+*/
+void initPop(int M, int N, vector<Individual>& population) {
+    //将种群中所有的染色体初始化为根号二分之一
+    population.resize(M);
+    for (int i = 0; i < M; i++) {
+        population[i] = Individual();
+    }
+}
+
+/*
+* 塌缩函数，完成对种群的一次测量
+*/
+void collapse(vector<Individual>& population) {
+    for (int i = 0; i < POP_SIZE; i++) {
+        string binTemp = "";
+        for (int j = 0; j < CHROM_LEN; j++) {
+             double pick = srand();
+             double alpha = population[i].getChrom()[j].alpha;
+             if (pick > alpha * alpha) {
+                 binTemp += '1';
+             }
+             else {
+                 binTemp += '0';
+             }
+        }
+        population[i].setBinary(binTemp);
+        //同时初始化specFlag;
+        population[i].setSpecFlag(0);
+        //population[i].setBinary("1111011111110100101111110100101101100110");
+    }
+}
+
+/*
+* 量子旋转门QGate RAS_1
+*   x	best 	f(x)>=f(best)	delta	    ab>0	ab<0	a=0	    b=0
+*   0	0		false			0		    0		0		0	    0
+*   0	0		true			0		    0		0		0	    0
+*   0	1		false			0   	    0		0		0	    0
+*   0	1		true			0.01pi	    -1		1		+-1	    0
+*   1	0		false			0.01pi	    -1		1		+-1	    0
+*   1	0		true			0.01pi	    1		-1		0	    +-1
+*   1	1		false			0.01pi	    1		-1		0	    +-1
+*   1	1		true			0.01pi	    1		-1		0	    +-1
+*/
+void qGateRAS_1(vector<Individual>& population, Individual& best) {
+    double bestFit = best.getFitness();                         //最优个体适应度
+    for (int i = 0; i < POP_SIZE; i++) {
+        for (int j = 0; j < CHROM_LEN; j++) {
+            double alpha = population[i].getChrom()[j].alpha;   //α
+            double beta = population[i].getChrom()[j].beta;     //β
+            char x = population[i].getBinary()[j];              //当前个体第j个编码
+            char b = best.getBinary()[j];                       //最优个体第j个编码
+            double delta = 0.0;                                 //旋转角大小
+            int s = 0;                                          //旋转角方向，即正负号
+            double curFit = population[i].getFitness();         //当前个体适应度
+            //当前个体与最优个体相同，不旋转
+            if ((x == '0' && b == '0')) {
+                delta = 0;
+                s = 0;
+            }
+            else if ((x == '0' && b == '1') || (curFit < bestFit)) {
+                delta = 0;
+                s = 0;
+            }
+            else if (x == '0' && b == '1' && curFit >= bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit < bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit >= bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            else if (x == '1' && b == '1' && curFit < bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            else if (x == '1' && b == '1' && curFit >= bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            double e = s * delta;       //旋转角
+            double newAlpha = alpha * cos(e) - beta * sin(e);
+            double newBeta = alpha * sin(e) + beta * cos(e);
+            population[i].setQubitByPos(j, newAlpha, newBeta);
+        }
+    }
+}
+
+/*
+* 量子旋转门QGate RAS_2
+*   x	best 	f(x)>f(best)	delta	    ab>0	ab<0	a=0	    b=0
+*   0	0		false			0		    0		0		0	    0
+*   0	0		true			0		    0		0		0	    0
+*   0	1		false			0.01pi	    1		-1		0	    +-1
+*   0	1		true			0.01pi	    -1		1		+-1	    0
+*   1	0		false			0.01pi	    -1		1		+-1	    0
+*   1	0		true			0.01pi	    1		-1		0	    +-1
+*   1	1		false			0		    0		0		0	    0
+*   1	1		true			0		    0		0		0	    0
+*/
+void qGateRAS_2(vector<Individual>& population, Individual& best) {
+    double bestFit = best.getFitness();                         //最优个体适应度
+    for (int i = 0; i < POP_SIZE; i++) {
+        for (int j = 0; j < CHROM_LEN; j++) {
+            double alpha = population[i].getChrom()[j].alpha;   //α
+            double beta = population[i].getChrom()[j].beta;     //β
+            char x = population[i].getBinary()[j];              //当前个体第j个编码
+            char b = best.getBinary()[j];                       //最优个体第j个编码
+            double delta = 0.0;                                 //旋转角大小
+            int s = 0;                                          //旋转角方向，即正负号
+            double curFit = population[i].getFitness();         //当前个体适应度
+            //当前个体与最优个体相同，不旋转
+            if ((x == '0' && b == '0') || (x == '1' && b == '1')) {
+                delta = 0;
+                s = 0;
+            }
+            else if ((x == '0' && b == '1') || ( curFit < bestFit)) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            else if (x == '0' && b == '1' && curFit >= bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit < bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit >= bestFit) {
+                delta = 0.01 * PI;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            double e = s * delta;       //旋转角
+            double newAlpha = alpha * cos(e) - beta * sin(e);
+            double newBeta = alpha * sin(e) + beta * cos(e);
+            population[i].setQubitByPos(j, newAlpha, newBeta);
+        }
+    }
+}
+
+/*
+* 量子旋转门QGate Adaptive
+*   x	best 	f(x)>=f(best)	delta	    ab>0	ab<0	a=0	    b=0
+*   0	0		false			0		    0		0		0	    0
+*   0	0		true			0		    0		0		0	    0
+*   0	1		false			Θ   	    1		-1		0	    +-1
+*   0	1		true			Θ   	    -1		1		+-1	    0
+*   1	0		false			Θ   	    -1		1		+-1	    0
+*   1	0		true			Θ   	    1		-1		0	    +-1
+*   1	1		false			0		    0		0		0	    0
+*   1	1		true			0		    0		0		0	    0
+* 其中，旋转角Θ的计算公式如下:
+* Θ = K1 + (K2 - K1) * (f_i - f_min) / (f_max - f_min)  [f_min != f_max]
+* Θ = K1                                                [f_min == f_max]
+* f_i、f_max、f_min分别戴代表当前个体的适应度、当前种群最优个体适应度以及最差适应度
+* K1、K2为两个正常数且K1 < K2，用于控制收敛速度
+*/
+void qGateAdaptive(vector<Individual>& population, Individual& best, double& f_max, double& f_min) {
+    const double CONST_ARG =(f_max > f_min) ?  (K2 - K1) / (f_max - f_min) : 0;
+
+    double bestFit = best.getFitness();                         //最优个体适应度
+    for (int i = 0; i < POP_SIZE; i++) {
+        //根据当前个体计算旋转角
+        double theta = K1 + (population[i].getFitness() - f_min) * CONST_ARG;
+        for (int j = 0; j < CHROM_LEN; j++) {
+            double alpha = population[i].getChrom()[j].alpha;   //α
+            double beta = population[i].getChrom()[j].beta;     //β
+            double delta = 0.0;                                 //旋转角大小
+            char x = population[i].getBinary()[j];              //当前个体第j个编码
+            char b = best.getBinary()[j];                       //最优个体第j个编码
+            int s = 0;                                          //旋转角方向，即正负号
+            double curFit = population[i].getFitness();         //当前个体适应度
+            //当前个体与最优个体相同，不旋转
+            if ((x == '0' && b == '0') || (x == '1' && b == '1')) {
+                delta = 0;
+                s = 0;
+            }
+            else if ((x == '0' && b == '1') || (curFit < bestFit)) {
+                delta = theta;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            else if (x == '0' && b == '1' && curFit >= bestFit) {
+                delta = theta;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit < bestFit) {
+                delta = theta;
+                if (alpha * beta > 0) {
+                    s = -1;
+                }
+                else if (alpha * beta < 0) {
+                    s = 1;
+                }
+                else if (alpha == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+                else if (beta == 0) {
+                    s = 0;
+                }
+            }
+            else if (x == '1' && b == '0' && curFit >= bestFit) {
+                delta = theta;
+                if (alpha * beta > 0) {
+                    s = 1;
+                }
+                else if (alpha * beta < 0) {
+                    s = -1;
+                }
+                else if (alpha == 0) {
+                    s = 0;
+                }
+                else if (beta == 0) {
+                    s = (srand() > 0.5) ? 1 : -1;
+                }
+            }
+            double e = s * delta;       //旋转角
+            if (e == 0) {
+                //H-ep门
+                if (alpha * alpha <= EPSLION && beta * beta >= 1 - EPSLION) {
+                    population[i].setQubitByPos(j, sqrt(EPSLION), sqrt(1 - EPSLION));
+                }
+                else if (beta * beta <= EPSLION && alpha * alpha >= 1 - EPSLION) {
+                    population[i].setQubitByPos(j, sqrt(1 - EPSLION), sqrt(EPSLION));
+                }
+            }
+            else {
+                double newAlpha = alpha * cos(e) - beta * sin(e);
+                double newBeta = alpha * sin(e) + beta * cos(e);
+                population[i].setQubitByPos(j, newAlpha, newBeta);
+            }
+            
+        }
+    }
+
+}
+
+/*
+* 解码二进制编码，返回十进制，并转移到变量所限制的区间
+* @param binary: 个体的二进制编码，包括多个基因
+* @param bound: 变量的上下限
+* @return: 各个基因（变量）的数组
+*/
+vector<double> decodeBinary(string binary, vector<range> bound) {
+    vector<double> x(GENE_NUM);
+    //binary = "1111011111110100101111110100101101100110";
+    for (int i = 0; i < binary.size(); i += GENE_LEN) {
+        int index = i / GENE_LEN;
+        string curGene = binary.substr(i, GENE_LEN);
+        double var = stoi(curGene, nullptr, 2);
+        //将变量值映射到对应的range区间
+        var = bound[index].floor + var / (pow(2, GENE_LEN) - 1) * (bound[index].ceil - bound[index].floor);
+        x[index] = static_cast<double>(var);
+        //cout << "x" << index << " = " << x[index] << endl;
+    }
+    return x;
+}
+
+/*
+* 待优化的目标函数如下：
+* f(x, y) = x * sin(4 * Pi * x) + y * sin(20 * Pi * y)
+* x:[-3.0, 12.1]
+* y:[4.1, 5.8]
+* @param binary：个体的二进制编码
+*/
+double objFunc(Individual& indv) {
+    string binary = indv.getBinary();
+    vector<double> x(GENE_NUM); //各个变量的数组
+    vector<range> bound = { range(-3.0, 12.1), range(4.1, 5.8) };
+    x = decodeBinary(binary, bound);
+    double fitness = x[0] * sin(4 * PI * x[0]) + x[1] * sin(20 * PI * x[1]);
+    indv.setGeneDec(x);
+    return fitness;
+}
+
+/*
+* Shaffer's Func:
+* 此函数有无限多个局部极大值点，其中只有一个(0, 0)为全局最大
+* x: [-10, 10]
+* y: [-10, 10]
+*/
+double objFuncShaffer(Individual& indv) {
+    string binary = indv.getBinary();
+    vector<double> x(GENE_NUM); //各个变量的数组
+    vector<range> bound = { range(-10, 10), range(-10, 10) };
+    x = decodeBinary(binary, bound);
+    double fitness = 0.5 - (pow(sin(sqrt(x[0] * x[0] + x[1] * x[1])), 2) - 0.5) / (pow(1 + 0.001 * (x[0] * x[0] + x[1] * x[1]), 2));
+    indv.setGeneDec(x);
+    return fitness;
+}
+
+/*
+* 适应度计算，计算种群中所有个体的适应度值
+* @param population: 种群
+*/
+void calFitness(vector<Individual>& population, Individual& best, double& f_max, double& f_min) {
+    double bestFit = -DBL_MAX;
+    double maxFit = -DBL_MAX;
+    double minFit = DBL_MAX;
+    if (best.getFitness() != 0) {
+        bestFit = best.getFitness();
+    }
+    int bestIdx = 0; //最优个体位置
+    int maxIdx = 0;  //f_max位置
+    int minIdx = 0;  //f_min位置
+    for (int i = 0; i < population.size(); i++) {
+        double fitness = objFuncShaffer(population[i]);
+        //记录最优个体
+        bestIdx = (fitness > bestFit) ? i : bestIdx;
+        bestFit = max(bestFit, fitness);
+        population[i].setFitness(fitness);
+        //记录本代中最优个体
+        maxIdx = (fitness > maxFit) ? i : maxIdx;
+        maxFit = max(maxFit, fitness);
+        //记录本代中最差个体
+        minIdx = (fitness < minFit) ? i : minIdx;
+        minFit = min(minFit, fitness);
+    }
+    //如果本代种群中有比最优个体适应度更高的个体，更新best
+    if (bestFit != best.getFitness()) {
+        best = population[bestIdx];
+    }
+    //记录最优个体
+    f_max = maxFit;
+    population[maxIdx].setSpecFlag(1);
+    //记录最差个体
+    f_min = minFit;
+    population[minIdx].setSpecFlag(2);
+}
+
+
+void printPopulation(vector<Individual>& pop) {
+    for (int i = 0; i < pop.size(); i++) {
+        cout << pop[i].toString() << endl;
+    }
+}
+
+/*
+* QGA主函数
+*/
+void quantumAlgorithm() {
+    Individual best;
+    vector<Individual> population;
+
+    //初始化种群
+    initPop(POP_SIZE, CHROM_LEN, population);
+    //对种群进行一次测量，得到二进制编码
+    collapse(population);
+    //计算适应度
+    double f_max = 0;
+    double f_min = 0;
+    calFitness(population, best, f_max, f_min);
+    //进化迭代
+    for (int gen = 0; gen < MAX_GEN; gen++) {
+        cout << "当前进化代数： " << gen << endl;
+        //测量种群
+        collapse(population);
+        //计算适应度
+        double f_max = 0;
+        double f_min = 0;
+        calFitness(population, best, f_max, f_min);
+        //量子旋转门
+        qGateAdaptive(population, best, f_max, f_min);
+        cout << "best chrom:\n" << best.toString() << endl;
+    }
+}
+
+int main()
+{
+    quantumAlgorithm();
+    
+    std::cout << "Hello World!\n";
+}
---
+++ QGA/QGA.h
@@ -0,0 +1,115 @@
+#pragma once
+#define _USE_MATH_DEFINES 
+#include <cmath>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <string>
+#include <float.h>
+using namespace std;
+
+const int MAX_GEN = 50;						//最大迭代次数
+const int POP_SIZE = 100;					//种群大小
+const int GENE_NUM = 2;						//基因个数，即变量的个数
+const int GENE_LEN = 21;					//每个基因的编码长度，即每个变量的二进制编码长度
+const int CHROM_LEN = GENE_NUM * GENE_LEN;	//个体的二进制编码长度
+const double INIT_AMPLITUDE = 1 / sqrt(2);	//根号二分之一常量，用于初始化种群
+const double PI = M_PI;						//Pi常量
+const double K1 = 0.001 * PI;				//最小旋转角
+const double K2 = 0.05 * PI;				//最大旋转角
+const double EPSLION = 0.1;					//H-ep门
+
+struct qubit {
+	double alpha;
+	double beta;
+	qubit() : alpha(INIT_AMPLITUDE), beta(INIT_AMPLITUDE) {}
+	qubit(double a, double b) : alpha(a), beta(b) {}
+};
+struct range {
+	double floor;
+	double ceil;
+	range(double x, double y) : floor(x), ceil(y) {}
+};
+
+class Individual {
+private:
+	vector<qubit> mChrom;	  //用qubit编码的染色体
+	double mFitness;		  //适应值
+	string mBinary;			  //二进制编码
+	vector<double> mGenesDec; //每个基因（变量）的十进制表示
+	int mSpecFlag;			  //记录特殊个体的标志（0：普通个体 1：最优个体 2：最差个体）
+public:
+	Individual() {
+		//无参构造函数，创建初始化种群
+		mChrom.resize(CHROM_LEN);
+		for (int i = 0; i < CHROM_LEN; i++) {
+			qubit initQ = qubit();
+			mChrom[i] = initQ;
+		}
+		this->mFitness = 0.0;
+		this->mBinary = "";
+		this->mGenesDec = {0, 0};
+		this->mSpecFlag = 0;
+	};
+	Individual(vector<qubit> chrom, double fitness, string binary) {
+		this->mChrom = chrom;
+		this->mFitness = fitness;
+		this->mBinary = binary;
+		this->mGenesDec = { 0, 0 };
+		this->mSpecFlag = 0;
+	}
+	vector<qubit> getChrom() {
+		return this->mChrom;
+	}
+	void setQubitByPos(int index, double a, double b) {
+		qubit q = qubit(a, b);
+		this->mChrom[index] = q;
+	}
+	double getFitness() {
+		return this->mFitness;
+	}
+	void setFitness(double fitness) {
+		this->mFitness = fitness;
+	}
+	vector<double> getGeneDec() {
+		return this->mGenesDec;
+	}
+	void setGeneDec(vector<double> geneDec) {
+		this->mGenesDec = geneDec;
+	}
+	int getSpecFlag() {
+		return this->mSpecFlag;
+	}
+	void setSpecFlag(int specFlag) {
+		this->mSpecFlag = specFlag;
+	}
+	string getBinary() {
+		return this->mBinary;
+	}
+	void setBinary(const string binary) {
+		this->mBinary = binary;
+	}
+	string toString() {
+		string ans = "";
+		////打印α
+		//ans += "qubit alpha :";
+		//for (qubit q : mChrom) {
+		//	ans += to_string(q.alpha);
+		//}
+		//ans += "\n";
+		////打印β
+		//ans += "qubit beta :";
+		//for (qubit q : mChrom) {
+		//	ans += to_string(q.beta);
+		//}
+		for (int i = 0; i < mGenesDec.size(); i++) {
+			ans += "x" + to_string(i) + " = " + to_string(mGenesDec[i]) + "\n";
+		}
+		//打印适应值
+		ans += "mFitness = " + to_string(mFitness);
+		ans += "\n";
+		//打印二进制编码
+		ans += "mBinary = " + mBinary + "\n";
+		return ans;
+	}
+};
