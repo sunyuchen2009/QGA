dc3b1487cb7a1c9e2cb42b3e06cedefe957b2cc0
syc <sunyuchen2009@163.com>
2023-05-01 20:02:56 +0800

    添加图着色数据，禁忌搜索算子完善

--- GCP_QGA/GCP_QGA.h
+++ GCP_QGA/GCP_QGA.h
@@ -3,6 +3,9 @@
 #define NORMAL 0	  //普通个体
 #define POP_BEST 1	  //种群中最优个体
 #define POP_WORST 2	  //种群中最差个体
+#define GA_FIT 0
+#define TS_FIT 1
+
 #include <cmath>
 #include <vector>
 #include <random>
@@ -10,20 +13,26 @@
 #include <string>
 #include <float.h>
 #include <set>
+#include <queue>
+#include <deque>
+#include <fstream>
 #include <unordered_set>
 using namespace std;
 
-const int MAX_GEN = 50;					//最大迭代次数
-const int POP_SIZE = 200;					//种群大小
-const int GENE_NUM = 25;						//基因个数，即变量的个数
-const int GENE_LEN = 4;						//每个基因的编码长度，即每个变量的二进制编码长度
-const int CHROM_LEN = GENE_NUM * GENE_LEN;	//个体的二进制编码长度
-const double MIGRATE_RATE = 0.1;			//移民比率
-const double INIT_AMPLITUDE = 1 / sqrt(2);	//根号二分之一常量，用于初始化种群
-const double PI = M_PI;						//Pi常量
-const double K1 = 0.001 * PI;				//最小旋转角
-const double K2 = 0.008 * PI;				//最大旋转角
-const double EPSLION = 0.1;					//H-ep门
+const int MAX_GEN = 5000;							//最大迭代次数
+const int TABU_GEN = 10;							//禁忌搜索最大迭代次数
+const int POP_SIZE = 200;							//种群大小
+const int GENE_NUM = 2;							//基因个数，即变量的个数
+const int GENE_LEN = 25;								//每个基因的编码长度，即每个变量的二进制编码长度
+const int CHROM_LEN = GENE_NUM * GENE_LEN;			//个体的二进制编码长度
+const double MIGRATE_RATE = 0.1;					//移民比率
+const double INIT_AMPLITUDE = 1 / sqrt(2);			//根号二分之一常量，用于初始化种群
+const double PI = M_PI;								//Pi常量
+const double K1 = 0.001 * PI;						//最小旋转角
+const double K2 = 0.008 * PI;						//最大旋转角
+const double EPSLION = 0.1;							//H-ep门
+
+static vector<pair<int, int>> GCP_EDGE;				//无向图边集合
 
 //量子比特
 struct qubit {
@@ -130,17 +139,17 @@ public:
 	}
 	string toString() {
 		string ans = "";
-		//打印α
-		ans += "qubit alpha :";
-		for (qubit q : mChrom) {
-			ans += to_string(q.alpha) + " ";
-		}
-		ans += "\n";
-		//打印β
-		ans += "qubit beta :";
-		for (qubit q : mChrom) {
-			ans += to_string(q.beta) + " ";
-		}
+		////打印α
+		//ans += "qubit alpha :";
+		//for (qubit q : mChrom) {
+		//	ans += to_string(q.alpha) + " ";
+		//}
+		//ans += "\n";
+		////打印β
+		//ans += "qubit beta :";
+		//for (qubit q : mChrom) {
+		//	ans += to_string(q.beta) + " ";
+		//}
 		ans += '\n';
 		for (int i = 0; i < mGenesDec.size(); i++) {
 			ans += "x" + to_string(i) + " = " + to_string(mGenesDec[i]) + "\n";
@@ -161,7 +170,19 @@ struct tabuItem {
 	pair<int, int> vertexs;	//对换的两个顶点位置
 	double fitDiff;			//对换后相较原个体提升的适应度
 	Individual newIndv;
+	tabuItem() : vertexs(pair<int, int>()), fitDiff(-1), newIndv(Individual()) {}
 	tabuItem(pair<int, int> v, double diff, Individual indv) : vertexs(v), fitDiff(diff), newIndv(indv) {}
+
+	bool operator==(const tabuItem& rhs)const {
+		if (vertexs.first == rhs.vertexs.first && vertexs.second == rhs.vertexs.second ||
+			vertexs.first == rhs.vertexs.second && vertexs.second == rhs.vertexs.first) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
 	string toString() {
 		string ans;
 		ans += "i = ";
